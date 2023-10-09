cc20f4f06ebb181aa9fa9e7dc7faaf95f98d17b4
Luke Carty <lukecarty161@gmail.com>
2023-10-09 11:51:24 +0100

    added a private route for shipping

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -22,6 +22,7 @@ import CartPage from "./pages/CartPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
 import ShippingPage from "./pages/ShippingPage.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -31,7 +32,10 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartPage />}></Route>
       <Route path="/login" element={<LoginPage />}></Route>
       <Route path="/register" element={<RegisterPage />}></Route>
-      <Route path="/shipping" element={<ShippingPage />}></Route>
+
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingPage />}></Route>
+      </Route>
     </Route>
   )
 );
