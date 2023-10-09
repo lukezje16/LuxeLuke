c540badd02fc002623c587ec56aa8eb594e7ea06
Luke Carty <lukecarty161@gmail.com>
2023-10-09 12:52:11 +0100

    added a checkout Nav for checkout steps

--- frontend/src/pages/ShippingPage.jsx
+++ frontend/src/pages/ShippingPage.jsx
@@ -5,6 +5,7 @@ import FormContainer from "../components/FormContainer";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { saveShippingAddress } from "../slices/cartSlice.js";
+import CheckoutSteps from "../components/CheckoutSteps";
 
 const ShippingPage = () => {
   //get cart from redux store
@@ -27,6 +28,7 @@ const ShippingPage = () => {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2></CheckoutSteps>
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address" className="my-2">
